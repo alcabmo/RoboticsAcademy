ec51b14c729754f4407579b477b60578be3e6cec
vanejessi <jessi_3118@hotmail.com>
2023-12-31 14:41:02 +0100

    Inicio version websocket camera

--- exercises/static/exercises/color_filter_newmanager/react-components/SpecificColorFilter.js
+++ exercises/static/exercises/color_filter_newmanager/react-components/SpecificColorFilter.js
@@ -2,10 +2,39 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import { drawImage} from "./helpers/showImagesColorFilter";
 
+// The stream & capture
+var stream = document.getElementById('stream');
+
+// The video stream
+var cameraStream = null;
+
 
 function SpecificColorFilter(props) {
   const [image, setImage] = React.useState(null)
   React.useEffect(() => {
+      var mediaSupport = 'mediaDevices' in navigator;
+
+    if( mediaSupport && null == cameraStream ) {
+
+        navigator.mediaDevices.getUserMedia({video: true})
+        .then(function(mediaStream) {
+
+            cameraStream = mediaStream;
+            stream.srcObject = mediaStream;
+            stream.play();
+        })
+        .catch(function(err) {
+
+            console.log("Unable to access camera: " + err);
+        });
+    }
+    else {
+
+        alert('Your browser does not support media devices.');
+
+        return;
+    }
+    requestAnimationFrame(showImageOutput);
     console.log("TestShowScreen subscribing to ['update'] events");
     const callback = (message) => {
       if(message.data.update.image_camera){
@@ -47,6 +76,10 @@ function SpecificColorFilter(props) {
       <canvas id="gui_canvas_camera"></canvas>
       <canvas id="gui_canvas"></canvas>
     </div>
+    <script src="https://webrtc.github.io/adapter/adapter-latest.js">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.6.5/dat.gui.min.js">
+    </script>
   );
 }
 
