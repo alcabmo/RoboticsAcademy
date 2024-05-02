3f1f01dd3d734fc71fd1f15de6ae9e85b1a756c9
vanejessi <jessi_3118@hotmail.com>
2024-05-02 10:27:04 +0200

    Rama con version humble de websocket camera

---
+++ exercises/static/exercises/color_filter_newmanager/react-components/SpecificColorFilter.js
@@ -0,0 +1,68 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import {drawImage, startStreaming} from "./helpers/showImagesColorFilter";
+// The stream & capture
+//var stream = document.getElementById('stream');
+
+
+function SpecificColorFilter(props) {
+  const [image, setImage] = React.useState(null)
+  React.useEffect(() => {
+    console.log("TestShowScreen subscribing to ['update'] events");
+    // Start Streaming
+    //startStreaming()
+    const callback = (message) => { 
+      if(message.data.update.image)
+      {
+        const image = JSON.parse(message.data.update.image)
+        if(image.image)
+        {
+          drawImage(message.data.update)
+        } 
+      }
+ 
+    };
+
+   /*return () => {
+        
+   // Start Streaming
+    startStreaming()
+    };*/
+   
+    window.RoboticsExerciseComponents.commsManager.subscribe(
+      [window.RoboticsExerciseComponents.commsManager.events.UPDATE],
+      callback
+    );
+
+    return () => {
+      console.log("TestShowScreen unsubscribing from ['state-changed'] events");
+      window.RoboticsExerciseComponents.commsManager.unsubscribe(
+        [window.RoboticsExerciseComponents.commsManager.events.UPDATE],
+        callback
+      );
+    };
+  }, []);
+
+
+
+  return (
+    <div style={{display: "flex",   width: "100%",
+    height: "100%"}}>
+    <canvas id="gui_canvas"></canvas>
+    </div>
+  );
+}
+
+
+SpecificColorFilter.propTypes = {
+  circuit: PropTypes.string,
+};
+
+/*setTimeout(function(){
+    console.log("START LAUNCHER");
+    //startStreaming();
+    UseCamera();
+    //declare_webrtcframe();
+}, 15000);*/
+
+export default SpecificColorFilter
---
+++ exercises/static/exercises/color_filter_newmanager/react-components/helpers/showImagesColorFilter.js
@@ -0,0 +1,112 @@
+
+// To decode the image string we will receive from server
+function decode_utf8(s){
+    return decodeURIComponent(escape(s))
+}
+
+let image = new Image();
+let image_camera = new Image();
+// The stream & capture
+var stream = document.getElementById('stream');
+// The video stream
+var cameraStream = null;
+
+export function drawImage (data){
+    var canvas = document.getElementById("gui_canvas"),
+    context = canvas.getContext('2d')
+
+    // For image object
+    image.onload = function(){
+        update_image();
+    }
+
+    // Request Animation Frame to remove the flickers
+    function update_image(){
+        window.requestAnimationFrame(update_image);
+        context.drawImage(image, 0, 0);
+    }
+            
+    // Parse the Image Data
+    var image_data = JSON.parse(data.image),
+        source = decode_utf8(image_data.image),
+        shape = image_data.shape;
+    
+    if(source != ""){
+        image.src = "data:image/jpeg;base64," + source;
+        canvas.width = shape[1];
+        canvas.height = shape[0];
+    }   
+}
+
+export function drawImageCamera (data){
+    var canvas_camera = document.getElementById("gui_canvas_camera"),
+    context_camera = canvas_camera.getContext('2d')
+
+    // For image object
+    image_camera.onload = function(){
+        update_camera_image();
+    }
+
+    // Request Animation Frame to remove the flickers
+    function update_camera_image(){
+        window.requestAnimationFrame(update_camera_image);
+        context_camera.drawImage(image_camera, 0, 0);
+    }
+            
+    // Parse the Image Data
+    var image_data = JSON.parse(data.image_camera),
+        source = decode_utf8(image_data.image_camera),
+        shape = image_data.shape;
+    
+    if(source != ""){
+        image_camera.src = "data:image/jpeg;base64," + source;
+        canvas_camera.width = shape[1];
+        canvas_camera.height = shape[0];
+    }   
+}
+
+// Start Streaming
+/*export function startStreaming() {
+
+console.log("startStreaming");
+    var mediaSupport = 'mediaDevices' in navigator;
+
+    if( mediaSupport && null == cameraStream ) {
+
+        window.navigator.mediaDevices.getUserMedia({video: true})
+        .then(function(mediaStream) {
+
+            cameraStream = mediaStream;
+            videoRef.current.srcObject = mediaStream;
+            videoRef.current.play();
+            console.log("stream play");
+
+        })
+        .catch(function(err) {
+
+            console.log("Unable to access camera: " + err);
+            //stopStreaming();
+        });
+    }
+    else {
+
+        alert('Your browser does not support media devices.');
+
+        return;
+    }
+    //requestAnimationFrame(showImageOutput);
+
+}*/
+
+/*function stopStreaming() {
+
+    if(null != cameraStream) {
+
+        var track = cameraStream.getTracks()[0];
+
+        track.stop();
+        stream.load();
+
+        cameraStream = null;
+    }
+}*/
